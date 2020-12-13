730b1ffa53a2d09211e48849e96d99f155916803
NJ-Brad <nj_brad@outlook.com>
2020-12-13 15:54:24 -0500

    Add project files.

---
+++ LinkViewerControl/ViewerControl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Markdig;
+
+namespace LinkViewerControl
+{
+    public partial class ViewerControl : UserControl
+    {
+        public ViewerControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnCreateControl()
+        {
+            base.OnCreateControl();
+            await webBrowser1.EnsureCoreWebView2Async();
+
+            if (!string.IsNullOrEmpty(pendingLink))
+            {
+                ShowLink(pendingLink);
+            }
+        }
+
+        string pendingLink = string.Empty;
+
+        public void ShowLink(string url)
+        {
+            try
+            {
+                pendingLink = url;
+                webBrowser1.CoreWebView2.Navigate(url);
+                pendingLink = string.Empty;
+            }
+            catch { }
+        }
+    }
+}
---
+++ TestApp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            viewerControl1.ShowLink("http://google.com");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            viewerControl1.ShowLink("http://google.com");
+
+        }
+    }
+}
